d6d118faf082fa05b58bca50dc6cff053c84fb37
Jeroen-TheCodingFactory <tcf@jeroen-rijsdijk.nl>
2023-05-22 12:26:44 +0200

    setup after first lesson

---
+++ js/main.js
@@ -0,0 +1,46 @@
+// Setup Classes
+class API{
+
+}
+
+class Decrypter{
+
+}
+
+class Encrypter{
+
+}
+
+class Cleaner{
+
+}
+
+class Renderer{
+
+}
+
+/* View classes */
+
+class Main{
+
+}
+
+class EncrypterView{
+
+}
+
+class DecrypterView{
+
+}
+
+class Header{
+
+}
+
+class Body{
+
+}
+
+class Footer{
+
+}
