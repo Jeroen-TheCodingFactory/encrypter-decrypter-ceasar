7efd5d6b2696042dddb70b6126e3d53d67ea02a1
Jeroen-TheCodingFactory <tcf@jeroen-rijsdijk.nl>
2023-05-22 12:30:48 +0200

    forgot the relations

--- js/main.js
+++ js/main.js
@@ -22,15 +22,39 @@ class Renderer{
 /* View classes */
 
 class Main{
+    encrypterView;
+    decrypterView;
 
+    constructor(){
+        this.encrypterView = new EncrypterView();
+        this.decrypterView = new DecrypterView();
+    }
 }
 
 class EncrypterView{
 
+    header;
+    body;
+    footer;
+
+    constructor(){
+        this.header = new Header();
+        this.body = new Body();
+        this.footer = new Footer();
+    }
 }
 
 class DecrypterView{
 
+    header;
+    body;
+    footer;
+
+    constructor(){
+        this.header = new Header();
+        this.body = new Body();
+        this.footer = new Footer();
+    }
 }
 
 class Header{
@@ -44,3 +68,24 @@ class Body{
 class Footer{
 
 }
+
+class App{
+    api;
+    decrypter;
+    encrypter;
+    cleaner;
+    renderer;
+    main;
+
+    constructor(){
+        this.api = new API();
+        this.decrypter = new Decrypter();
+        this.encrypter = new Encrypter();
+        this.cleaner = new Cleaner();
+        this.renderer = new Renderer();
+        this.main = new Main();
+    }
+}
+
+const app = new App();
+console.log(app);
